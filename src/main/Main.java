f876dd59910581e84ca050b93788bd436bd4afeb
Curtis Madsen <ckmadsen@bu.edu>
2017-04-07 16:32:25 -0400

    Updated tool to allow for the customization of the confidence interval.

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,6 +16,8 @@ import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.ParserRuleContext;
 
+import org.apache.commons.math3.distribution.NormalDistribution;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -24,35 +26,38 @@ public class Main {
 				if (args[0].equals("-h") || args[0].equals("--h") || args[0].equals("-help")
 						|| args[0].equals("--help")) {
 					System.out
-							.println("Usage:  java -jar SMC4STL-<version>.jar <directory_of_csv_traces> <property_to_check>");
+							.println("Usage:  java -jar SMC4STL-<version>.jar <confidence_interval> <directory_of_csv_traces> <property_to_check>");
 					return;
 				}
-				String property = args[1];
-				for (int i = 2; i < args.length; i++) {
-					property += args[i];
-				}
-				File dir = new File(args[0]);
-				if (!dir.isDirectory()) {
-					System.out.println("First argument must be a directory of CSV trace files.");
-				}
-				else {
-					List<String> traceFiles = new ArrayList<String>();
-					for (File file : dir.listFiles()) {
-						if (file.getName().endsWith(".csv") || file.getName().endsWith(".CSV")) {
-							traceFiles.add(file.getAbsolutePath());
-						}
+				if (args.length > 2) {
+					String property = args[2];
+					for (int i = 3; i < args.length; i++) {
+						property += args[i];
 					}
-					double satisfyingPercent = computeSatisfyingPercent(traceFiles, property);
-					System.out.println((satisfyingPercent * 100) + "% of traces satisfy the property.");
-					System.out.println("The true satisfying percentage is within "
-							+ (computeError(satisfyingPercent, traceFiles.size()) * 100)
-							+ "% of this value with 95% confidence.");
+					double confidenceInterval = Double.parseDouble(args[0]);
+					File dir = new File(args[1]);
+					if (!dir.isDirectory()) {
+						System.out.println("Second argument must be a directory of CSV trace files.");
+					}
+					else {
+						List<String> traceFiles = new ArrayList<String>();
+						for (File file : dir.listFiles()) {
+							if (file.getName().endsWith(".csv") || file.getName().endsWith(".CSV")) {
+								traceFiles.add(file.getAbsolutePath());
+							}
+						}
+						double satisfyingPercent = computeSatisfyingPercent(traceFiles, property);
+						System.out.println((satisfyingPercent * 100) + "% of traces satisfy the property.");
+						System.out.println("The true satisfying percentage is within "
+								+ (computeError(satisfyingPercent, traceFiles.size(), confidenceInterval) * 100)
+								+ "% of this value with " + (confidenceInterval*100) + "% confidence.");
+					}
 				}
 			}
 			else {
 				System.out.println("Incorrect number of arguments.");
 				System.out
-						.println("Usage:  java -jar SMC4STL-<version>.jar <directory_of_csv_traces> <property_to_check>");
+						.println("Usage:  java -jar SMC4STL-<version>.jar <confidence_interval> <directory_of_csv_traces> <property_to_check>");
 			}
 		}
 		catch (IOException e) {
@@ -80,8 +85,10 @@ public class Main {
 		return ((double) sat) / ((double) traces.size());
 	}
 
-	public static double computeError(double satisfyingPercent, int numTraces) {
-		return 1.96 * Math.sqrt((1 - satisfyingPercent) / (satisfyingPercent * numTraces));
+	public static double computeError(double satisfyingPercent, int numTraces, double confidenceInterval) {
+		NormalDistribution distribution = new NormalDistribution(0, 1);
+		double confidenceValue = distribution.inverseCumulativeProbability(confidenceInterval + ((1 - confidenceInterval) / 2));
+		return confidenceValue * Math.sqrt((1 - satisfyingPercent) / (satisfyingPercent * numTraces));
 	}
 
 }
